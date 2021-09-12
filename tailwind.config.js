5082d8ad2b5eb4daea7353617831207e9fe6b90e
Oscar Beaumont <oscar@otbeaumont.me>
2021-09-12 10:35:19 +0800

    feat: :sparkles: responsive design

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,15 @@
 module.exports = {
   mode: "jit",
   purge: ["./public/**/*.html", "./src/**/*.{astro,svelte,js,jsx,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      minWidth: {
+        "32px": "32px",
+      },
+      screens: {
+        ttiny: "473px",
+        tiny: "530px",
+      },
+    },
+  },
 };
