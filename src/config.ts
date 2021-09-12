1269f02eaa4ab9ac42298dce9238e00dfb0e0eb4
Oscar Beaumont <oscar@otbeaumont.me>
2021-09-12 09:07:56 +0800

    fix: :bug: fixed portfolio images not loading + move config to dedicated file

---
+++ src/config.ts
@@ -0,0 +1,43 @@
+export default {
+    title: "Oscar Beaumont",
+    description: "I'm a freelance software developer based in Perth, Australia with a huge passion for technology. I create solutions to help people use technology effectively.",
+    lang: "en_US",
+    socialMedia: [
+        {
+            name: "GitHub",
+            icon_url: "../assets/github.png",
+            url: "https://github.com/oscartbeaumont"
+        },
+        {
+            name: "LinkedIn",
+            icon_url: "../assets/linkedin.png",
+            url: "https://www.linkedin.com/in/oscartbeaumont"
+        },
+    ],
+    skills: {
+        "Languages": {
+            "Go": "https://golang.org",
+            "Rust": "https://www.rust-lang.org",
+            "Typescript": "https://www.typescriptlang.org",
+            "Dart": "https://dart.dev",
+            "Java (University level, no real world experience)": "https://www.java.com",
+        },
+        "Frontend": {
+            "React": "https://reactjs.org",
+            "Svelte": "https://svelte.dev",
+            "Relay": "https://relay.dev",
+        },
+        "Application Development": {
+            "Electron": "https://www.electronjs.org",
+            "Flutter": "https://flutter.dev",
+            "Bloc": "https://bloclibrary.dev",
+        },
+        "Systems Administration/Devops": {
+            "Linux": undefined,
+            "Kubernetes": "https://kubernetes.io",
+        },
+        "Protocols": {
+            "Windows Mobile Device Management Protocol": "https://docs.microsoft.com/en-us/windows/client-management/mdm",
+        }
+    }
+};
