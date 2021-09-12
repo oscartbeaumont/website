ec3ffad800dca85310e26f6796646990e9426964
Oscar Beaumont <oscar@otbeaumont.me>
2021-09-12 12:00:12 +0800

    feat: :sparkles: assets page

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,6 @@ module.exports = {
   purge: ["./public/**/*.html", "./src/**/*.{astro,svelte,js,jsx,ts,tsx,vue}"],
   theme: {
     extend: {
-      minWidth: {
-        "32px": "32px",
-      },
       screens: {
         ttiny: "473px",
         tiny: "530px",
