391becbbbe6682abe64d60d20e0ba98d4ac074c9
Oscar Beaumont <oscar@otbeaumont.me>
2022-06-17 23:07:37 +0800

    fix: upgrade Astro + other deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,18 @@
 // @ts-check
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 
 /** @type {import('astro').AstroUserConfig} */
 export default {
+  site: "https://otbeaumont.me",
   integrations: [
+    sitemap(),
     svelte(),
     tailwind({
       config: {
         path: "./tailwind.config.js",
-        applyAstroPreset: false,
       },
     }),
   ],
-  buildOptions: {
-    site: "https://otbeaumont.me",
-    sitemap: true,
-  },
 };
