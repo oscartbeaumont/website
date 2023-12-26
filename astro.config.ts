5ec96fc2c5fec219c23448a90c3550e3bab1878f
Oscar Beaumont <oscar@otbeaumont.me>
2023-12-26 21:53:10 +0800

    Remove old stuff + cleanup

--- astro.config.ts
+++ astro.config.ts
@@ -2,18 +2,12 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 import solidJs from "@astrojs/solid-js";
-import type { Plugin } from "vite";
-import { promises as fs } from "node:fs";
-import { createRequire } from "module";
 import vercel from "@astrojs/vercel/static";
 import mdx from "@astrojs/mdx";
-const cjs = createRequire(import.meta.url);
-const sharp: typeof import("sharp") = cjs("sharp");
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://otbeaumont.me",
-  srcDir: "src/astro",
   compressHTML: true,
   adapter: vercel(),
   integrations: [
@@ -24,46 +18,4 @@ export default defineConfig({
     solidJs(),
     mdx(),
   ],
-  vite: {
-    plugins: [imageOptimiserPlugin()],
-  },
 });
-
-function imageOptimiserPlugin(): Plugin {
-  const virtualModuleId = "virtual:optimiser";
-  const resolvedVirtualModuleId = "\0" + virtualModuleId;
-
-  return {
-    name: "transform-file",
-
-    resolveId(id) {
-      const url = new URL(`http://localhost/${id}`);
-      if (url.searchParams.has("optimise")) {
-        return resolvedVirtualModuleId;
-      }
-    },
-
-    async transform(src, id) {
-      const url = new URL(`http://localhost/${id}`);
-      if (url.searchParams.has("optimise")) {
-        const fileRaw = await fs.readFile(id.split("?")[0]);
-
-        var output;
-        const size = url.searchParams.get("size");
-        if (size) {
-          const [height, width] = size.split(",");
-          output = await sharp(fileRaw)
-            .webp()
-            .resize(Number(height), Number(width))
-            .toBuffer();
-        } else {
-          output = await sharp(fileRaw).webp().toBuffer();
-        }
-
-        return `export default "data:image/webp;base64,${output.toString(
-          "base64"
-        )}"`;
-      }
-    },
-  };
-}
--- src/config.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  title: "Oscar Beaumont",
-  description:
-    "I'm a freelance software developer based in Perth, Australia with a huge passion for technology. I create solutions to help people use technology effectively.",
-  lang: "en_US",
-} as const;
