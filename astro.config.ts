3082dd1108feb54bded9e567141a56a290016d3a
Oscar Beaumont <oscar@otbeaumont.me>
2025-02-23 22:54:23 +0800

    upgrade deps (including Tailwind v4)

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,10 @@
-import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 import solidJs from "@astrojs/solid-js";
 import vercel from "@astrojs/vercel";
 import mdx from "@astrojs/mdx";
 import remarkCodeTitles from "remark-code-titles";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,15 +13,11 @@ export default defineConfig({
   output: "static",
   adapter: vercel(),
   markdown: { remarkPlugins: [remarkCodeTitles] },
-  integrations: [
-    sitemap(),
-    tailwind({
-      configFile: "./tailwind.config.js",
-    }),
-    solidJs(),
-    mdx(),
-  ],
+  integrations: [sitemap(), solidJs(), mdx()],
   server: {
     port: 3008,
+  },
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
