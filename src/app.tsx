947637bbeb06fd5113fca080db5337294cc4e489
Oscar Beaumont <oscar@otbeaumont.me>
2025-10-31 20:13:43 +0800

    opengraph + meta prototype

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,7 @@
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
+import { DynamicImage, OpenGraph } from '@solid-mediakit/og';
+import { Link, MetaProvider, Title } from "@solidjs/meta";
 import { Suspense } from "solid-js";
 import "./app.css";
 
@@ -7,9 +9,20 @@ export default function App() {
   return (
     <Router
       root={props => (
-        <>
+        <MetaProvider>
           <Suspense>{props.children}</Suspense>
-        </>
+
+          {/* TODO: Finish SEO + OpenGraph Metadata -> + dynamic configuration of it using `constants.ts` style file */}
+          <Title>Oscar Beaumont</Title>
+          <Link rel="canonical" href="http://otbeaumont.me" />
+
+          {/*TODO: Dynamic domain*/}
+          <OpenGraph origin='https://otbeaumont.me'>
+            <DynamicImage>
+              <div>This is very cool!</div>
+            </DynamicImage>
+          </OpenGraph>
+        </MetaProvider>
       )}
     >
       <FileRoutes />
--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,12 @@ import { solidStart } from "@solidjs/start/config";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import { cloudflare } from "@cloudflare/vite-plugin";
+import { vitePlugin as OGPlugin } from "@solid-mediakit/og/unplugin";
 import { headersPlugin } from "./src/vite-plugin-headers";
 
 export default defineConfig({
   plugins: [
+    OGPlugin(),
     solidStart({
       middleware: "./src/middleware.ts"
     }) as any,
