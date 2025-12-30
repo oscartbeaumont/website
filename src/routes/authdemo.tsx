9f2a4ceee9787a793e954ccd5312fb6825413794
Oscar Beaumont <oscar@otbeaumont.me>
2025-12-30 19:03:30 +0800

    attempt to get auth working

---
+++ src/routes/authdemo.tsx
@@ -0,0 +1,30 @@
+import { getRequestEvent } from "solid-js/web";
+import { HttpHeader, HttpStatusCode } from "@solidjs/start";
+import { createResource } from "solid-js";
+import { redirect } from "@solidjs/router";
+
+async function todo() {
+  "use server";
+
+  const auth = getRequestEvent()?.request.headers.get("Authorization");
+  if (!auth)
+    throw redirect("/login");
+
+  return "bruh";
+}
+
+export default function Page() {
+  const [data] = createResource(() => todo(), { deferStream: true })
+
+  // const auth = getRequestEvent()?.request.headers.get("Authorization");
+  // if (!auth) {
+  //   return <>
+  //     <HttpStatusCode code={401} />
+  //     <HttpHeader name="WWW-Authenticate" value="Basic realm='admin'" />
+  //     <p>Unauthenticated</p>
+  //   </>;
+  // }
+
+  // TODO: Logout?
+  return <p>Authenticated {data()}</p>
+}
