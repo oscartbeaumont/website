ec2a4501d4ad409e52cf80744addcb72a93a1ac9
Oscar Beaumont <oscar@otbeaumont.me>
2026-01-02 16:55:28 +0800

    capture exceptions with PostHog

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -1,12 +1,15 @@
 // @refresh reload
 import { mount, StartClient } from "@solidjs/start/client";
+import "posthog-js/dist/exception-autocapture";
 import posthog from "posthog-js/dist/module.no-external";
 
-if (!import.meta.env.DEV)
-	posthog.init("phc_p4ybAvaYqJojhuZD0dVGExGoCCqbWViGzlxQc0AuWUv", {
-		api_host: "/ph_4DkU",
-		ui_host: "https://us.posthog.com",
-		person_profiles: "identified_only",
-	});
+posthog.init("phc_p4ybAvaYqJojhuZD0dVGExGoCCqbWViGzlxQc0AuWUv", {
+	api_host: "/ph_4DkU",
+	ui_host: "https://us.posthog.com",
+	defaults: "2025-05-24",
+	person_profiles: "identified_only",
+	capture_exceptions: true,
+	debug: import.meta.env.DEV,
+});
 
 mount(() => <StartClient />, document.getElementById("app")!);
