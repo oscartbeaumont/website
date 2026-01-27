5681a5f426e69b3567101c1579357ec61730ec09
Oscar Beaumont <oscar@otbeaumont.me>
2026-01-27 16:13:04 +0800

    disable PostHog in development

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -2,14 +2,15 @@
 import { mount, StartClient } from "@solidjs/start/client";
 import "posthog-js/dist/exception-autocapture";
 import posthog from "posthog-js/dist/module.no-external";
+import { isServer } from "solid-js/web";
 
-posthog.init("phc_p4ybAvaYqJojhuZD0dVGExGoCCqbWViGzlxQc0AuWUv", {
-	api_host: "/ph_4DkU",
-	ui_host: "https://us.posthog.com",
-	defaults: "2025-05-24",
-	person_profiles: "identified_only",
-	capture_exceptions: true,
-	debug: import.meta.env.DEV,
-});
+if (!isServer && !import.meta.env.DEV)
+	posthog.init("phc_p4ybAvaYqJojhuZD0dVGExGoCCqbWViGzlxQc0AuWUv", {
+		api_host: "/ph_4DkU",
+		ui_host: "https://us.posthog.com",
+		defaults: "2025-05-24",
+		person_profiles: "identified_only",
+		capture_exceptions: true,
+	});
 
 mount(() => <StartClient />, document.getElementById("app")!);
